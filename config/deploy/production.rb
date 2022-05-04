34a650396cabff90f57720432fe589eec95016c8
truefiremedia <94484182+truefiremedia@users.noreply.github.com>
2022-05-04 14:29:39 -0500

    changed ssh options

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -41,11 +41,11 @@ server "165.227.121.241", user: "deploy", roles: %w{app db web}
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/user_name/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+ set :ssh_options, {
+   keys: %w(/home/deploy/.ssh/id_rsa),
+   forward_agent: true,
+   auth_methods: %w(publickey)
+ }
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
