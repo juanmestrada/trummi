71ba32f5e2e7c5c30199c018f02ef781003085d9
truefiremedia <94484182+truefiremedia@users.noreply.github.com>
2022-05-05 15:23:26 -0500

    yarn env

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,10 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bund
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, {
+   PATH: '$HOME/.npm-packages/bin/:$PATH',
+   NODE_ENVIRONMENT: 'production'
+}
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
