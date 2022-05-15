bb4dae40132b511545b2780333a32324db289815
truefiremedia <94484182+truefiremedia@users.noreply.github.com>
2022-05-14 19:04:52 -0500

    path_style fog

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -15,11 +15,12 @@
 #   }
 # end
 CarrierWave.configure do |config|
-config.fog_credentials = {
-:provider => 'AWS',
-:aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-:region => ENV['AWS_REGION'] #This is usually required for European Users not all
-}
-config.fog_directory = ENV['S3_BUCKET_NAME']
+	config.fog_credentials = {
+		:provider => 'AWS',
+		:aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+		:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+		:region => ENV['AWS_REGION'],
+		:path_style            => true
+	}
+	config.fog_directory = ENV['S3_BUCKET_NAME']
 end
