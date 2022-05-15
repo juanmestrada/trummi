ee2ae1ac891fe086e9f73ac0ab2fde6e5961f7c4
truefiremedia <94484182+truefiremedia@users.noreply.github.com>
2022-05-15 13:04:19 -0500

    aws endpoint

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -16,12 +16,12 @@
 # end
 CarrierWave.configure do |config|
 	config.fog_credentials = {
-		:provider => 'AWS',
-		:aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-		:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-		:region => ENV['AWS_REGION'],
-		:path_style            => true
+		:provider 				=> 'AWS',
+		:aws_access_key_id 		=> ENV['AWS_ACCESS_KEY_ID'],
+		:aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
+		:region                 => 'us-west-1',
+    	:endpoint               => 'https://s3-us-west-1.amazonaws.com/'
 	}
-	config.fog_public = false
+
 	config.fog_directory = ENV['S3_BUCKET_NAME']
 end
